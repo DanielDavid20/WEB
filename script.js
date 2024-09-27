27b864d1768a5ec53f14680cf9b97a81f67c0053
DanielDavid20 <danielmoralesdoria14@gmail.com>
2024-09-26 19:21:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript para el video destacado
+
+const video = document.querySelector('iframe');
+
+video.addEventListener('click', () => {
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) { // Firefox
+        video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) { // IE/Edge
+        video.msRequestFullscreen();
+    }
+});
